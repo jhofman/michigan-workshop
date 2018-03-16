20cd2feb9f520fdee62bee6290701236e1ce02e1
Jake Hofman <jhofman@gmail.com>
2018-03-16 08:13:40 -0400

    lecture 1

---
+++ michigan.R
@@ -0,0 +1,3 @@
+my_packages <- c("tidyverse", "modelr", "e1071", "glmnet", "caret", "scales", "broom", "ElemStatLearn", "ROCR", "tm", "Matrix", "class")
+
+install.packages(my_packages, repos = "http://cran.rstudio.com")
